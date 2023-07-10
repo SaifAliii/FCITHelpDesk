c593bc6fe6c6e45f6157050dc38a117e1303e24f
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-07-10 07:39:41 +0500

    FCIT HELP DESK

---
+++ Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace WebApplication1.Hubs
+{
+    public class MyHub: Hub
+    {
+        public async Task sendMessage(string message)
+        {
+            await Clients.All.SendAsync("Recieve Message", message);
+
+        }
+    }
+}
